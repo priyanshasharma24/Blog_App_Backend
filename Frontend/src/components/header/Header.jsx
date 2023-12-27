e8b5f25dd967698bbc32738ae620bc3792c6e16b
priyanshasharma24 <138128911+priyanshasharma24@users.noreply.github.com>
2023-12-27 15:29:01 +0530

    Update Header.jsx

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className='header'>
       <div className="headerTitles">
-        <span className="headerTitleSm">Laravel</span>
+        <span className="headerTitleSm">React</span>
         <span className="headerTitleLg">Blog</span>
       </div>
       <img className="headerImg"
